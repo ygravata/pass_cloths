24bb653239899e4b09f8ddec990c77671ac4d073
Yuri Gravata <ygravata@gmail.com>
2021-03-17 20:46:00 -0300

    running migration and creating seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_223716) do
+ActiveRecord::Schema.define(version: 2021_03_17_232953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_223716) do
   create_table "cattles", force: :cascade do |t|
     t.string "cattle_name"
     t.string "politician"
-    t.string "type"
+    t.integer "cattle_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning Database.."
+
+PassCloth.destroy_all
+Phrase.destroy_all
+Cattle.destroy_all
+
+bolsominion = Cattle.create(
+  cattle_name: "Bolsominion",
+  politician: "Bolsonaro",
+  cattle_type: 1
+)
+
+puts "Creating #{bolsominion.cattle_name.capitalize}!"
+
+petista = Cattle.create(
+  cattle_name: "Petista",
+  politician: "Lula",
+  cattle_type: 2
+)
+
+puts "Creating #{petista.cattle_name.capitalize}!"
+
+common_sentences = [
+  "A Globo tá de sacaganagem, sabotando o governo",
+  "Também, com esse judiciário e congresso quem consegue governar?",
+  "Tá reclamando do que? Se muda do país"
+]
+
+bolso_sentences = [
+  "Deixa o homem trabalhar!",
+  "Nosso presidente tá fazendo o que é necessário pro país dar certo!",
+  "Se não faz politicagem, vocês reclamam! Se faz, vocês reclamam!",
+  "Ele tá fazendo isso porque teve que deixar o inimigo agir pra poder mostrar ao que veio!",
+  "Rachadinha todo mundo faz, mas só pegam no pé porque é filho dele.",
+  "Bom era na época do PT né?"
+]
+
+bolso_sentences.each do |sentence|
+  phrase = Phrase.create(
+    text: sentence
+  )
+
+  pass_cloth = PassCloth.create(
+    cattle_id: bolsominion.id,
+    phrase_id: phrase.id
+  )
+
+  puts "Phrase #{phrase.id} created!"
+
+end
+
+common_sentences.each do |sentence|
+  phrase = Phrase.create(
+    text: sentence
+  )
+
+  pass_cloth = PassCloth.create(
+    cattle_id: bolsominion.id,
+    phrase_id: phrase.id
+  )
+
+  pass_cloth = PassCloth.create(
+    cattle_id: petista.id,
+    phrase_id: phrase.id
+  )
+
+  puts "Phrase #{phrase.id} created!"
+
+end

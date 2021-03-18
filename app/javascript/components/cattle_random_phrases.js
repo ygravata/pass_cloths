4f61533a0e5866b93074e12f3d1b5e68cf295478
Yuri Gravata <ygravata@gmail.com>
2021-03-18 15:26:24 -0300

    adding js on click action

---
+++ app/javascript/components/cattle_random_phrases.js
@@ -0,0 +1,40 @@
+import Rails from '@rails/ujs';
+
+const generateRandomCattlePhrase = () => {
+  const bolsoButton = document.querySelector("#bolsominion");
+  const petistaButton = document.querySelector("#petista");
+
+  document.body.addEventListener('click', event => {
+    if (event.target !== bolsoButton && event.target !== petistaButton) {
+      return
+    }
+      console.log(event.target.attributes.data.value);
+      const randomPhrase = document.querySelector("#random_phrase");
+      fetch(`/cattles/${event.target.attributes.data.value}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data.text);
+          randomPhrase.innerHTML = data.text;
+        });
+
+
+
+  })
+}
+
+export { generateRandomCattlePhrase };
+
+
+
+// Rails.ajax({
+//   url: `/cattles/${event.target.attributes.data.value}`,
+//   type: "GET",
+//   success: function (data) {
+//     console.log(data);
+//   }
+// });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import { generateRandomCattlePhrase } from '../components/cattle_random_phrases';
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -31,4 +34,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  generateRandomCattlePhrase();
 });

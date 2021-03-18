cfc1c33c62292aa6cb402a7583429a59a9791768
Yuri Gravata <ygravata@gmail.com>
2021-03-18 19:18:07 -0300

    adding class methods to models

--- app/models/cattle.rb
+++ app/models/cattle.rb
@@ -12,4 +12,13 @@
 class Cattle < ApplicationRecord
   has_many :pass_cloths
   has_many :phrases, through: :pass_cloths
+
+  def only_one_owner
+    array = []
+    self.phrases.each do |phrase|
+      array << phrase unless phrase.cattles_count == 2
+    end
+    array
+  end
+
 end
--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -10,4 +10,17 @@
 class Phrase < ApplicationRecord
   has_many :pass_cloths
   has_many :cattles, through: :pass_cloths
+
+  def cattles_count
+    self.cattles.count
+  end
+
+  def self.two_owners
+    phrases = Phrase.all
+    result = []
+    phrases.each do |phrase|
+      result << phrase if phrase.cattles_count == 2
+    end
+    result
+  end
 end
